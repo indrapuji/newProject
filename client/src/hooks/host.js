ad59debdcf036fa07e996d77bae56aa067f3f941
Indra Puji Novirwan <indrapuji@gmail.com>
2020-09-25 02:01:58 +0700

    heroku server

--- client/src/hooks/host.js
+++ client/src/hooks/host.js
@@ -1,5 +1,5 @@
 // const host = "http://localhost:3001";
-// const host = "https://jatisejahtera-cms.herokuapp.com";
-const host = "http://dev.jatisejahtera.xyz:3001";
+const host = "https://jatisejahtera-cms.herokuapp.com";
+// const host = "http://dev.jatisejahtera.xyz:3001";
 
 export default host;
