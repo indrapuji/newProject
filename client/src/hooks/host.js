c646ff5c86cff1ff85148ba76291e47562e89798
Indra Puji Novirwan <indrapuji@gmail.com>
2020-09-25 03:26:34 +0700

    merge server and change host

--- client/src/hooks/host.js
+++ client/src/hooks/host.js
@@ -1,5 +1,5 @@
 // const host = "http://localhost:3001";
 // const host = "https://jatisejahtera-cms.herokuapp.com";
-const host = "http://dev.jatisejahtera.xyz";
+const host = "https://dev.jatisejahtera.xyz";
 
 export default host;
