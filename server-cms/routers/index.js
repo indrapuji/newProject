44ebff0855fb5ee8d58f2b90269d46bad499b132
akbarakma <akbarajo1234@gmail.com>
2020-12-07 02:16:20 +0700

    Add download router

--- server-cms/routers/index.js
+++ server-cms/routers/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const user = require('./user');
 const claim_data = require('./claim_data');
 const content = require('./content');
+const createError = require("http-errors");
 
 router.use('/users', user);
 router.use('/data', claim_data);
@@ -9,6 +10,7 @@ router.use('/content', content);
 router.get("/download", (req, res, next) => {
   try {
     const { file } = req.query;
+    if (!file) throw createError(400, "File required");
     res.download(`./uploads/${file}`);
   } catch (err) {
     next(err);
