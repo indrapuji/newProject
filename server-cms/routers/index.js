b939c33be505b0a0d9e8c1bd92f123c6fcb122fe
akbarakma <akbarajo1234@gmail.com>
2020-12-07 02:28:51 +0700

    Fix bug

--- server-cms/routers/index.js
+++ server-cms/routers/index.js
@@ -12,7 +12,7 @@ router.get("/download", (req, res, next) => {
   try {
     const { file } = req.query;
     if (!file) throw createError(400, "File required");
-    if (!fs.existsSync(file)) throw createError(404, "File not found");
+    if (!fs.existsSync(`./uploads/${file}`)) throw createError(404, "File not found");
     res.download(`./uploads/${file}`);
   } catch (err) {
     next(err);
