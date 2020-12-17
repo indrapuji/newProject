89b07aef60d285f60833ec0f8979ba4d6737a017
akbarakma <akbarajo1234@gmail.com>
2020-12-18 00:52:47 +0700

    OKE dah

--- server-cms/seeding/convert.js
+++ server-cms/seeding/convert.js
@@ -5,18 +5,27 @@ const fs = require("fs");
 if (fs.existsSync("seeding.json")) fs.unlinkSync("seeding.json");
 let result = excelToJson({
   source: fs.readFileSync("Cata G Form combine till 1 september 2020.xlsx"),
-  sheets: ["Compilation - Done"],
+  sheets: ["Sheet1"],
   columnToKey: {
     "*": "{{columnHeader}}",
   },
 });
 
-result = result["Compilation - Done"].filter((data) => {
+result = result["Sheet1"].filter((data) => {
   if (data["Nama Pensiunan"] && data["NIP/NPP/NIK"]) {
     return data;
   }
 });
-// result = result["Compilation - Done"];
+// result = result["Sheet1"];
 result.shift();
+
+const seen = new Set();
+result = result.filter(el => {
+  const duplicate = seen.has(el["Nama Pensiunan"]);
+  seen.add(el["Nama Pensiunan"]);
+  return !duplicate;
+});
+
+
 result = JSON.stringify(result, null, 4);
 fs.writeFileSync("seeding.json", result, "utf8");
