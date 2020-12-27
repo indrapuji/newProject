ea98e24e196b1dcb1548c49aa5c5c66fa5041bf0
Indra Puji Novirwan <indrapuji@gmail.com>
2020-12-27 16:21:37 +0700

    host

--- client-cms/src/hooks/host.js
+++ client-cms/src/hooks/host.js
@@ -1,6 +1,6 @@
 // const host = "http://128.199.238.147:3001";
 // const host = "https://jatisejahtera-cms.herokuapp.com";
 // const host = 'http://localhost:3001'
-const host = "https://dev.jatisejahtera.or.id/";
+const host = 'https://dev.jatisejahtera.or.id';
 
 export default host;
