0deab14aa755dc063d6fae6230a2a49e76903f45
akbarakma <akbarajo1234@gmail.com>
2020-12-29 22:27:04 +0700

    tambah

--- server-cms/app.js
+++ server-cms/app.js
@@ -11,8 +11,8 @@ const app = express();
 
 app.use('/uploads', express.static('uploads'));
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.get('/files/all', (req, res) => {
 	let html = '<ul>';
